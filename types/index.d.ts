b3da9837b02098b0413dfaf853f91e4da6417eae
Scott Davis <sc.ttdav.s@gmail.com>
2023-04-21 16:01:33 -0400

    clean up a bit

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,22 +1,21 @@
-import { type Component, type ReactElement, type FunctionComponentElement, type ProviderProps, type FC, type PropsWithChildren } from "react";
-
-type Models = Record<string, object>
-
-type MapModelToProps<M extends Models, P extends object> = (models: M, props: P) => Partial<P>;
+import { type FunctionComponentElement, type ProviderProps, type PropsWithChildren } from "react";
 
 interface Options {
   debounce?: boolean,
   events?: Record<string, Array<string>>
-  modelTypes?: Models,
+  modelTypes?: Record<string, object>,
   withRef?: boolean
 }
 
-export declare function connectBackboneToReact<M extends Models = Models, P extends object = {}>(mapModelToProps: MapModelToProps<M, P>, options?: Options): (component: FC<P> | Component<P>) => (subcomponent: Partial<P>) => JSX.Element
+export declare function connectBackboneToReact<Model extends {}, ModelProps extends {}>(
+  mapModelToProps: (models: Model, props: ModelProps) => Partial<ModelProps>, options?: Options
+): <CombinedProps>(
+  wrappedComponent: React.ComponentType<CombinedProps>
+) => React.ComponentType<Omit<CombinedProps, keyof ModelProps>>;
+
 
 interface BackboneProviderProps<M> {
-  children: ReactElement
   models: M
 }
 
-// do we need generics here?
-export declare function BackboneProvider<M extends Models>(props: BackboneProviderProps<M>): FunctionComponentElement<ProviderProps<M>>;
+export declare function BackboneProvider<Models extends {}>(props: PropsWithChildren<BackboneProviderProps<Models>>): FunctionComponentElement<ProviderProps<Models>>;
